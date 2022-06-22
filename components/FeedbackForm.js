b29b2fb447d54dad9872204a9d88639ae6aa095a
amarsan13 <68304994+amarsan13@users.noreply.github.com>
2022-06-22 15:23:54 +0200

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -17,15 +17,15 @@ export default function FeedbackForm() {
             </label>
         </p>
   
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">Nombre</label>
         <input id="name" className={styles['form-field']} type="text" name="name" />
 
         <label htmlFor="email">Email</label>
         <input id="email" className={styles['form-field']} type="email" name="email" required />
 
-        <label htmlFor="feedback">What is your feedback?</label>
+        <label htmlFor="feedback">Que quieres preguntarnos?</label>
         <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
-        <button className={styles.button} type="submit">Submit</button>
+        <button className={styles.button} type="submit">Enviar</button>
       </form>
   )
 }
